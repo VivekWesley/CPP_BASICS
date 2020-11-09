827064001a88a0bee7bd78172bde47bb2438c9cb
VivekWesley <vivekjohnwesley@gmail.com>
2020-11-09 23:14:00 +0530

    ADD prime range using function

---
+++ function_print_prime_range.cpp
@@ -0,0 +1,46 @@
+// function to print prime range
+
+// sample output:
+// pick a starting point
+// 10
+// pick ending point
+// 20
+// 11
+// 13
+// 17
+// 19
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int num)
+{
+    for (int i = 2; i < sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int a;
+    int b;
+    cout << "pick a starting point" << endl;
+    cin >> a;
+    cout << "pick ending point" << endl;
+    cin >> b;
+
+    for (int i = a; i < b; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
