0dd93eb7726388e21c3a3dbdfef746592a486b6c
VivekWesley <vivekjohnwesley@gmail.com>
2021-01-10 10:45:21 +0530

    ADD sum of n numbers using recursion program

---
+++ sumOfNRecursion.cpp
@@ -0,0 +1,31 @@
+// sum of n numbers using recursion
+
+// sample output:
+// enter n value:
+// 4
+// ans: 10
+
+#include <iostream>
+using namespace std;
+
+int sumOfN(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    int prevSum = sumOfN(n - 1);
+    return n + prevSum;
+}
+
+int main()
+{
+    int n;
+    cout << "enter n value: " << endl;
+    cin >> n;
+
+    cout << "ans: " << sumOfN(n) << endl;
+
+    return 0;
+}
