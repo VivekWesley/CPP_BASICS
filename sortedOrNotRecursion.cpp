17ded4c10ff37e7d48a2e7c5dbd577392aba0cf4
VivekWesley <vivekjohnwesley@gmail.com>
2021-01-14 15:10:52 +0530

    ADD check given array sorted or not using recursion

---
+++ sortedOrNotRecursion.cpp
@@ -0,0 +1,28 @@
+// check given array is sorted or not using recursion.
+
+// sample output:
+// 1
+
+#include <iostream>
+using namespace std;
+
+bool sorted(int arr[], int n)
+{
+    if (n == 1)
+    {
+        return true;
+    }
+
+    bool restArray = sorted(arr + 1, n - 1);
+
+    return (arr[0] < arr[1] && restArray);
+}
+
+int main()
+{
+    int n;
+    int arr[] = {1, 2, 3, 4, 5};
+
+    cout << sorted(arr, 5) << endl;
+    return 0;
+}
