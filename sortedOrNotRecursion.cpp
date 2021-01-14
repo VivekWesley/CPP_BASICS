eb6a2ea5598f4565eee4fca0f18748c15ad99110
VivekWesley <vivekjohnwesley@gmail.com>
2021-01-14 16:16:46 +0530

    FIX error in the code

--- sortedOrNotRecursion.cpp
+++ sortedOrNotRecursion.cpp
@@ -3,6 +3,7 @@
 // sample output:
 // 1
 
+// #1
 #include <iostream>
 using namespace std;
 
@@ -14,15 +15,12 @@ bool sorted(int arr[], int n)
     }
 
     bool restArray = sorted(arr + 1, n - 1);
-
     return (arr[0] < arr[1] && restArray);
 }
 
 int main()
 {
-    int n;
     int arr[] = {1, 2, 3, 4, 5};
-
     cout << sorted(arr, 5) << endl;
     return 0;
 }
